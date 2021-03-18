afbe1bf530837fae548fde1f85b29591a895f232
Tobias Frauenfelder <fratobia@student.ethz.ch>
2021-03-18 17:07:46 +0100

    Hello from Group 8

--- src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
@@ -21,7 +21,7 @@ public class Application {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String helloWorld() {
-        return "The application is running.";
+        return "Hello from Group 8.";
     }
 
     @Bean
