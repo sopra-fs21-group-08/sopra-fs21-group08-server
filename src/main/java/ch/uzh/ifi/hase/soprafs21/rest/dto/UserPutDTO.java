a188254b80b8cccc8d4640bad352422e46ad4f28
dombosfi <filip.dombos@gmail.com>
2021-03-31 18:13:14 +0200

    added userPutDTO to .git

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/UserPutDTO.java
@@ -0,0 +1,41 @@
+package ch.uzh.ifi.hase.soprafs21.rest.dto;
+
+public class UserPutDTO {
+
+    private long id;
+    private String username;
+    private String token;
+    private String dob;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
